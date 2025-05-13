a8f5cb85307364e9f776d18694d8414b363bc3e6
Sanskar <sanskar.mathur@beehyv.com>
2025-05-14 00:26:12 +0530

    @JsonValue getter and fromValue helps in correct (value-based) mapping of Enums from workflow JSONs

--- src/main/java/com/winflow/flowcore/core/enums/WorkflowStatusEnum.java
+++ src/main/java/com/winflow/flowcore/core/enums/WorkflowStatusEnum.java
@@ -1,5 +1,7 @@
 package com.winflow.flowcore.core.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
 
 @Getter
@@ -12,5 +14,20 @@ public enum WorkflowStatusEnum {
 
     WorkflowStatusEnum(String status) {
         this.status = status;
+    }
+
+    @JsonValue
+    public String getStatus() {
+        return status;
+    }
+
+    @JsonCreator
+    public static WorkflowStatusEnum fromValue(String value) {
+        for (WorkflowStatusEnum statusEnum : values()) {
+            if (statusEnum.status.equalsIgnoreCase(value)) {
+                return statusEnum;
+            }
+        }
+        throw new IllegalArgumentException("Unknown enum value: " + value);
     }
 }
