6ff43fd5cdd4de07edbd6f4369d2365ec8192445
Sanskar <sanskar.mathur@beehyv.com>
2025-05-14 00:40:19 +0530

    Empty execute function added for removing errors. Needs Definition

--- src/main/java/com/winflow/flowcore/engine/WorkflowExecutor.java
+++ src/main/java/com/winflow/flowcore/engine/WorkflowExecutor.java
@@ -1,4 +1,10 @@
 package com.winflow.flowcore.engine;
 
+import com.winflow.flowcore.core.model.Workflow;
+import org.springframework.stereotype.Component;
+
+@Component
 public class WorkflowExecutor {
+    public void execute(Workflow workflow) {
+    }
 }
