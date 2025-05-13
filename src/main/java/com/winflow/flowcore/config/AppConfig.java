450eb124b2f425c630289b93cf6983a3130651cf
Sanskar <sanskar.mathur@beehyv.com>
2025-05-14 00:49:25 +0530

    Basic Configuration for custom ThreadPool for CronBasedTriggerHandler

--- src/main/java/com/winflow/flowcore/config/AppConfig.java
+++ src/main/java/com/winflow/flowcore/config/AppConfig.java
@@ -1,11 +1,22 @@
 package com.winflow.flowcore.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 @Configuration
 public class AppConfig {
 
     @Value("${workflow.directory.path:src/main/resources/workflows/}")
     public String workflowDirectory;
+
+    @Bean
+    public ThreadPoolTaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(10);
+        scheduler.setThreadNamePrefix("workflow-cron-");
+        scheduler.initialize();
+        return scheduler;
+    }
 }
