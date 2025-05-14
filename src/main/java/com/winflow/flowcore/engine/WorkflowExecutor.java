1a8076fd2871cee64ca31185e95f3378bdc73b47
Sanskar Mathur <sanskar.mathur@beehyv.com>
2025-05-14 11:55:48 +0530

    Auto-gen code to verify correct triggering of workflows.

--- src/main/java/com/winflow/flowcore/engine/WorkflowExecutor.java
+++ src/main/java/com/winflow/flowcore/engine/WorkflowExecutor.java
@@ -1,10 +1,20 @@
 package com.winflow.flowcore.engine;
 
+import com.winflow.flowcore.core.model.Job;
 import com.winflow.flowcore.core.model.Workflow;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
+@Slf4j
 public class WorkflowExecutor {
     public void execute(Workflow workflow) {
+        List<Job> jobs = workflow.getJobs();
+
+        for (Job job : jobs) {
+            log.info("{}: {}", job.getId(), job.getCommand());
+        }
     }
 }
