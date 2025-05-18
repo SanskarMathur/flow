32a0dce3181d4b2fc672fc91bf6dcf532f54c3ec
Sanskar <sanskar.mathur@beehyv.com>
2025-05-18 18:01:41 +0530

    Created JobExecutionException

---
+++ src/main/java/com/winflow/flowcore/exception/JobExecutionException.java
@@ -0,0 +1,13 @@
+package com.winflow.flowcore.exception;
+
+import lombok.Getter;
+
+@Getter
+public class JobExecutionException extends RuntimeException {
+    private final String jobId;
+
+    public JobExecutionException(String jobId, String message) {
+        super(message);
+        this.jobId = jobId;
+    }
+}
